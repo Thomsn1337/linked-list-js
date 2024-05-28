7cd1043177e77ab5a22a1b9248d341121e45b151
Thomas Steifler <steiflerthomas@gmail.com>
2024-05-28 16:36:58 +0200

    add node and list classes

---
+++ linkedList.js
@@ -0,0 +1,17 @@
+class ListNode {
+    /**
+     * @param {any} value
+     */
+    constructor(value) {
+        this.value = value || null;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+}
+
+export default LinkedList;
