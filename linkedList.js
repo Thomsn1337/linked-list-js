1645f726237c7ba48156be024393aee6ef3d6ded
Thomas Steifler <steiflerthomas@gmail.com>
2024-05-28 16:37:28 +0200

    add method to append values to the list

--- linkedList.js
+++ linkedList.js
@@ -12,6 +12,25 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    /**
+     * @param {any} value
+     */
+    append(value) {
+        const node = new ListNode(value);
+
+        if (this.head === null) {
+            this.head = node;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+
+        current.next = node;
+    }
 }
 
 export default LinkedList;
